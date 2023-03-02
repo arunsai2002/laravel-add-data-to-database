13b9fc43b778673926092eaa03876bb93487bce8
arunsai2002 <92771726+arunsai2002@users.noreply.github.com>
2023-03-02 16:37:18 +0530

    Create users.blade.php

---
+++ users.blade.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+
+    <title>Hello, world!</title>
+  </head>
+  <body>
+    <form action="" method="post">
+        <div class="form-group">
+          email:
+          <input type="email" name="email">
+          
+        </div>
+        <div class="form-group">
+          password:
+          <input type="password" name="password">
+        </div>
+        
+        <button type="submit" class="btn btn-primary">Submit</button>
+      </form>
+   </body>
+</html>
